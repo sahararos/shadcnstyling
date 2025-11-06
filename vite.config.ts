170f35e65cf3c112585c4cfdda7a7b5409b230fa
sahararos <sahararosin@gmail.com>
2025-11-06 11:41:09 +0000

    change repo

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/<shadcnstyling>/',
+  base: '/shadcnstyling/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
